939651d82e2fef9d42ab0978be0002224d34863f
Joao-Moura <jaummoura15@gmail.com>
2022-06-27 11:44:38 -0300

    Conversão de tipos e utilização de métodos genéricos

--- app/AEstrela.hs
+++ app/AEstrela.hs
@@ -1,67 +1,43 @@
 module AEstrela (menorCaminho) where
 
 import Data.Heap
-import Utils (mapaAtual, Pos, Mapa)
+import Data.List
+import Graphics.Gloss
 
-type Valor = (Int, Pos)
+import Map
 
-adjs :: [Pos]
-adjs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+type Valor = (Int, Point)
 
-insereHeap :: Pos -> Int -> Int -> MinPrioHeap Int Valor -> MinPrioHeap Int Valor
+insereHeap :: Point -> Float -> Int -> MinPrioHeap Float Valor -> MinPrioHeap Float Valor
 insereHeap posInicial valorF valorG heap =
-    insert (valorF, (valorG, posInicial)) heap
+    Data.Heap.insert (valorF, (valorG, posInicial)) heap
 
-menorCaminho :: Pos -> Pos -> [Pos]
+menorCaminho :: Point -> Point -> [Point]
 menorCaminho posInicial posFinal = caminho
     where
         mapa = mapaAtual
-        heap = insereHeap posInicial 0 0 (empty :: MinPrioHeap Int Valor)
+        heap = insereHeap posInicial 0 0 (empty :: MinPrioHeap Float Valor)
         caminho = aEstrela mapa [] [] posInicial posFinal heap
 
-mapaHeight = 31
-mapaWidth = 28
-mapaLength =
-    mapaHeight * mapaWidth
-
-coord2Index :: Pos -> Int
-coord2Index (x, y) =
-    mapaWidth * y + x
-
-isCellFree :: Mapa -> [Pos] -> Pos -> Bool
-isCellFree mapa visitados (x, y)
-    -- | index < 0 || index >= mapaLength || pos `elem` visitados = False
-    | index < 0 || index >= mapaLength = False
-    | (x, y) `elem` visitados = False
-    -- | otherwise = mapa !! index > 0
-    | otherwise = mapa !! y !! x > 0
-    where
-        index = coord2Index (x, y)
-        -- index = mapa !! fst pos !! snd pos
-
-freeAdjs :: Mapa -> Pos -> [Pos] -> [Pos]
-freeAdjs mapa (x, y) visitados =
-    Prelude.filter (\coord -> isCellFree mapa visitados coord) $ map (\(i, j) -> (x+i, y+j)) adjs
-
-calcDist :: Pos -> Pos -> Mapa -> Int
+calcDist :: Point -> Point -> Mapa -> Float
 calcDist (xInicial, yInicial) (xFinal, yFinal) mapa =
-    ((xFinal - xInicial) ^ 2) + ((yFinal - yInicial) ^ 2) + mapa !! yInicial !! xInicial
+    ((xFinal - xInicial) ^ 2) + ((yFinal - yInicial) ^ 2) + getCellValue (xInicial, yInicial)
 
-calcCaminho :: Mapa -> [Pos] -> Pos -> Pos -> MinPrioHeap Int Valor -> (Pos, MinPrioHeap Int Valor)
+calcCaminho :: Mapa -> [Point] -> Point -> Point -> MinPrioHeap Float Valor -> (Point, MinPrioHeap Float Valor)
 calcCaminho mapa visitados posInicial posFinal heap = do
     let Just ((_, (gAtual, selecionado)), heapPop) = view heap
-    let adjsLivres = freeAdjs mapa posInicial visitados
+    let adjsLivres = (freeAdjsPoints posInicial) \\ visitados
     let novaHeap = foldl (\heap adj -> insereHeap adj (calcDist adj posFinal mapa) (gAtual + 1) heap) heapPop adjsLivres
     (selecionado, novaHeap)
 
-aEstrela :: Mapa -> [Pos] -> [Pos] -> Pos -> Pos -> MinPrioHeap Int Valor -> [Pos]
+aEstrela :: Mapa -> [Point] -> [Point] -> Point -> Point -> MinPrioHeap Float Valor -> [Point]
 aEstrela mapa caminho visitados posInicial posFinal heap
     | selecionado == posFinal = novoCaminho
     | not isVazia = aEstrela mapa novoCaminho novoVisitados selecionado posFinal novaHeap
     | otherwise = error "Caminho impossivel de ser encontrado"
     where
         isVazia = Data.Heap.null heap
-        (selecionado, novaHeap) = calcCaminho mapa caminho visitados posInicial posFinal heap
+        (selecionado, novaHeap) = calcCaminho mapa visitados posInicial posFinal heap
         novoCaminho
             | not (selecionado `elem` caminho) = caminho ++ [selecionado]
             | otherwise = caminho
