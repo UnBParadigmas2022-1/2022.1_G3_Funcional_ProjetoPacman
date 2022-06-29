752490ab3e238fa5dac8a2d986e8faab3df89f2f
Thiago Paiva <thiagohdaqw@gmail.com>
2022-06-29 12:58:49 -0300

    Movendo tipos para arquivo Types

--- app/Types.hs
+++ app/Types.hs
@@ -1,5 +1,15 @@
 module Types where
 
+import Graphics.Gloss
+
 
 type CellSize = Float
 type Width    = Float
+
+type Cell = Float
+type Mapa = [Cell]
+
+type Wall = Picture
+type Assets = [Wall]
+
+type Game = (CellSize, Width, Mapa, Assets)
