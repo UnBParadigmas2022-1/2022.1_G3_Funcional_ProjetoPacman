2c34d1e264ed92d2a801c6533bf05daf79929e00
Lucas Boaventura <lucasxboaventura@hotmail.com>
2022-06-30 19:36:47 -0300

    Adicionando gerador random de  posições das moedas

--- app/Coin.hs
+++ app/Coin.hs
@@ -2,9 +2,18 @@ module Coin where
 
 import Graphics.Gloss
 
+import System.Random
+
 import Types
 
-drawCoin :: Float -> (Float, Float) -> [Picture]
-drawCoin cellSize (x, y) =
-    [translate (x * cellSize) (y * cellSize) 
+-- drawCoin :: Float -> (Float, Float) -> [Picture]
+drawCoin :: (p -> m a1) -> (a2, b) -> [Picture]
+drawCoin cellSize (x, y) = 
+    [translate (positionX * cellSize) (positionY * cellSize) 
     $ rectangleSolid cellSize cellSize]
+    where
+        positionX = generateCoinPosition
+        positionY = generateCoinPosition
+
+-- generateCoinPosition :: (Random a, Control.Monad.IO.Class.MonadIO m, Num a) => p -> m a
+generateCoinPosition a = randomRIO (0,  31)
