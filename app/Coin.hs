ec70dc78df9fc2ac446003e45f3dd0c9b2d21668
Lucas Boaventura <lucasxboaventura@hotmail.com>
2022-06-30 15:33:22 -0300

    Adicionando modulo de coin com a função para desenhar a moeda na tela

--- app/Coin.hs
+++ app/Coin.hs
@@ -1,1 +1,10 @@
 module Coin where
+
+import Graphics.Gloss
+
+import Types
+
+drawCoin :: Float -> (Float, Float) -> [Picture]
+drawCoin cellSize (x, y) =
+    [translate (x * cellSize) (y * cellSize) 
+    $ rectangleSolid cellSize cellSize]
