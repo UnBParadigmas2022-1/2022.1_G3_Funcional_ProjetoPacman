1a66411f7799321e270be5099d96fd2cff5205d1
DafneM <moretti.dafne2@gmail.com>
2022-06-30 20:36:21 -0300

    Adicionando algoritmo AEstrela na movimentação do Ghost

--- app/Ghost.hs
+++ app/Ghost.hs
@@ -2,9 +2,12 @@ module Ghost where
 
 import Graphics.Gloss
 import Types
+import AEstrela
 
 drawGhost :: Assets -> Float -> Ghost -> Picture
 drawGhost [_, _, diamond, _] cellSize (x,y) = color yellow $ translate (cellSize*x) (cellSize*(y)) $ diamond
 
 updateGhost :: Ghost -> Ghost
-updateGhost (x,y) = (x+1,y)
+updateGhost (x,y) = (newX,-newY)
+    where
+        (newX, newY) = AEstrela.aStar (x,abs(y)) (26,29)
