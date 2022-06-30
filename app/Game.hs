89d8ff2da71cca87e2a5b76f8c75ce52e1d41e21
Lucas Boaventura <lucasxboaventura@hotmail.com>
2022-06-30 15:34:55 -0300

    Adicionando uso da função de desenhar a coin na tela no updateGame

--- app/Game.hs
+++ app/Game.hs
@@ -5,13 +5,18 @@ import Graphics.Gloss.Interface.Pure.Game
 
 import Types
 import Map
+import Coin
 
 
 drawGame :: Game -> Picture
-drawGame (cellSize, width, mapa, assets) = pictures (Map.drawMapa assets cellSize width mapa (0, 0))
+drawGame (cellSize, width, mapa, assets, (x, y)) = 
+    pictures $ (Map.drawMapa assets cellSize width mapa (0, 0)) ++ coin
+    where
+        coin = drawCoin cellSize (x, y)
 
 updateGame :: Float -> Game -> Game
-updateGame dt game = game
+updateGame dt (cellSize, width, mapa, assets, (x, y)) = 
+    (cellSize, width, mapa, assets, (x + 1, y))
 
 inputHandler :: Event -> Game -> Game
 inputHandler _ g = g
