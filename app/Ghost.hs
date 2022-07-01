ddea18a42894715eb4e719e71013b7ec480e9dd7
Thiago Paiva <thiagohdaqw@gmail.com>
2022-07-01 10:35:14 -0300

    Adicionando BFS ao jogo

--- app/Ghost.hs
+++ app/Ghost.hs
@@ -3,6 +3,7 @@ module Ghost where
 import Graphics.Gloss
 import Types
 import AEstrela
+import Bfs
 
 drawGhost :: Assets -> Float -> Ghost -> Picture
 drawGhost [_, _, _, _, orangeGhost] cellSize (x,y) = color yellow $ translate (cellSize*x) (cellSize*(y)) $ orangeGhost
@@ -10,7 +11,9 @@ drawGhost [_, _, _, _, orangeGhost] cellSize (x,y) = color yellow $ translate (c
 updateGhost :: Ghost -> Algorithm -> Ghost
 updateGhost (x,y) algo = (newX, -newY)
     where
+        point = (x,abs(y))
         (newX, newY)
-            | algo == ASTAR = AEstrela.aStar (x,abs(y)) (26,29)
-            | otherwise = AEstrela.aStar (x,abs(y)) (26,29)
+            | algo == ASTAR = AEstrela.aStar point (26,29)
+            | algo == BFS = Bfs.bShortestPath point (26,29)
+            | otherwise = AEstrela.aStar point (26,29)
 
