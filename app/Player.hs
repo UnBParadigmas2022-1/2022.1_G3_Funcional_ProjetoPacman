bcc72ee05afb6997aa5b73f6381dab74c699a767
igorq937 <igorq937@gmail.com>
2022-07-01 18:38:51 -0300

    Adicionando input do player

--- app/Player.hs
+++ app/Player.hs
@@ -12,7 +12,16 @@ drawPlayer cellSize ((x, y), (_, _)) =
     (translate (cellSize*x) (-cellSize*y) $ color white $ rectangleSolid cellSize cellSize) : []
 
 updatePlayer :: Game -> Game
-updatePlayer game = game
+updatePlayer (cellSize, width, mapa, assets, ((x, y), (sx, sy))) =
+    (cellSize, width, mapa, assets, ((x+sx, y+sy), (sx, sy)))
 
 inputPlayer :: Event -> Game -> Game
+inputPlayer (EventKey (SpecialKey KeyUp) Down _ _) (cellSize, width, mapa, assets, ((x, y), (_, _))) =
+    (cellSize, width, mapa, assets, ((x, y), (0, -1)))
+inputPlayer (EventKey (SpecialKey KeyDown) Down _ _) (cellSize, width, mapa, assets, ((x, y), (_, _))) =
+    (cellSize, width, mapa, assets, ((x, y), (0, 1)))
+inputPlayer (EventKey (SpecialKey KeyRight) Down _ _) (cellSize, width, mapa, assets, ((x, y), (_, _))) =
+    (cellSize, width, mapa, assets, ((x, y), (1, 0)))
+inputPlayer (EventKey (SpecialKey KeyLeft) Down _ _) (cellSize, width, mapa, assets, ((x, y), (_, _))) =
+    (cellSize, width, mapa, assets, ((x, y), (-1, 0)))
 inputPlayer _ game = game
