119f9553f061cb06dc5616103ce9a044a8a83e64
Lucas Boaventura <lucasxboaventura@hotmail.com>
2022-07-01 15:35:44 -0300

    Adicionando lógica para pegar uma posição livre e desenhar a coin

--- app/Coin.hs
+++ app/Coin.hs
@@ -2,21 +2,35 @@ module Coin where
 
 import Graphics.Gloss
 
-import System.Random ( randomRs, mkStdGen )
+import System.Random
+import System.IO.Unsafe
+
+import Map
 
 import Types
-
-drawCoin :: Float -> (Float, Float) -> [Picture]
--- drawCoin :: (p -> m a1) -> (a2, b) -> [Picture]
-drawCoin cellSize (x, y) = 
-    [translate (x * cellSize) (y * cellSize)
-    $ rectangleSolid cellSize cellSize]
-    -- where
-    --     positionX = generateCoinPosition
-    --     positionY = generateCoinPosition cellSize
+import GHC.Float (int2Float)
 
 
--- rndPoints1 :: a -> [Int]
--- generateCoinPosition _ = head x
---   where
---     x = randomRs (1, 31) mkStdGen (1)
+drawCoin :: Float -> Assets -> [Picture]
+drawCoin cellSize [_, _, _, _, coin] =
+    [translate positionX positionY $ coin]
+    where
+        (newX, newY) = (10, 10)
+        positionX = newX * cellSize
+        positionY = newY * cellSize
+
+
+generateRandom :: Point -> (Float, Float)
+generateRandom (x, y) = 
+    (unsafePerformIO (getStdRandom (randomR (0, 28))),  
+    unsafePerformIO (getStdRandom (randomR (0, 31))))
+
+
+-- nextPosition :: Point
+-- nextPosition
+--     | cellFree = random
+--     | otherwise = nextPosition
+--     where
+--         (x, y) = generateRandom (0, 31)
+--         random = (int2Float (round x), int2Float(round y))
+--         cellFree = isCellFree random
