2000e46b1d98e40e1255b2101fe01439d0cdecf9
Joao-Moura <jaummoura15@gmail.com>
2022-07-01 19:22:01 -0300

    Corrigindo backtracking do A*

--- app/AEstrela.hs
+++ app/AEstrela.hs
@@ -7,44 +7,39 @@ import Graphics.Gloss
 import Map
 import Types
 
+type Valor = (Int, MultValor)
 
-type Valor = (Int, Point)
-
-aStar posInicial posFinal
-    | posInicial == posFinal = posInicial
-    | otherwise = (menorCaminho posInicial posFinal) !! 1
-
-insereHeap :: Point -> Float -> Int -> MinPrioHeap Float Valor -> MinPrioHeap Float Valor
-insereHeap posInicial valorF valorG heap =
-    Data.Heap.insert (valorF, (valorG, posInicial)) heap
-
-menorCaminho :: Point -> Point -> [Point]
-menorCaminho posInicial posFinal = caminho
+aStar posInicial posFinal = posProx
     where
         mapa = mapaAtual
-        heap = insereHeap posInicial 0 0 (empty :: MinPrioHeap Float Valor)
-        caminho = aEstrela mapa [] [] posInicial posFinal heap
+        heap = insereHeap posInicial posInicial 0 0 (empty :: MinPrioHeap Float Valor)
+        caminho = aEstrela mapa [] [] (posInicial, posInicial) posFinal heap
+        posProx = geraProximo (reverse caminho) posInicial
+
+insereHeap :: Point -> Point -> Float -> Int -> MinPrioHeap Float Valor -> MinPrioHeap Float Valor
+insereHeap posInicial posPai valorF valorG heap =
+    Data.Heap.insert (valorF, (valorG, (posInicial, posPai))) heap
 
 calcDist :: Point -> Point -> Mapa -> Float
 calcDist (xInicial, yInicial) (xFinal, yFinal) mapa =
-    ((xFinal - xInicial) ^ 2) + ((yFinal - yInicial) ^ 2) + getCellValue (xInicial, yInicial)
+    sqrt (((xFinal - xInicial) ^ 2) + ((yFinal - yInicial) ^ 2)) + getCellValue (xInicial, yInicial)
 
-calcCaminho :: Mapa -> [Point] -> Point -> Point -> MinPrioHeap Float Valor -> (Point, MinPrioHeap Float Valor)
+calcCaminho :: Mapa -> [Point] -> Point -> Point -> MinPrioHeap Float Valor -> (MultValor, MinPrioHeap Float Valor)
 calcCaminho mapa visitados posInicial posFinal heap = do
     let Just ((_, (gAtual, selecionado)), heapPop) = view heap
     let adjsLivres = (freeAdjsPoints posInicial) \\ visitados
-    let novaHeap = foldl (\heap adj -> insereHeap adj (calcDist adj posFinal mapa) (gAtual + 1) heap) heapPop adjsLivres
+    let novaHeap = foldl (\heap adj -> insereHeap adj posInicial (calcDist adj posFinal mapa) (gAtual + 1) heap) heapPop adjsLivres
     (selecionado, novaHeap)
 
-aEstrela :: Mapa -> [Point] -> [Point] -> Point -> Point -> MinPrioHeap Float Valor -> [Point]
-aEstrela mapa caminho visitados posInicial posFinal heap
+aEstrela :: Mapa -> [MultValor] -> [Point] -> MultValor -> Point -> MinPrioHeap Float Valor -> [MultValor]
+aEstrela mapa caminho visitados (posInicial, _) posFinal heap
     | selecionado == posFinal = novoCaminho
-    | not isVazia = aEstrela mapa novoCaminho novoVisitados selecionado posFinal novaHeap
+    | not isVazia = aEstrela mapa novoCaminho novoVisitados (selecionado, posInicial) posFinal novaHeap
     | otherwise = error "Caminho impossivel de ser encontrado"
     where
         isVazia = Data.Heap.null heap
-        (selecionado, novaHeap) = calcCaminho mapa visitados posInicial posFinal heap
+        ((selecionado, pai), novaHeap) = calcCaminho mapa visitados posInicial posFinal heap
         novoCaminho
-            | not (selecionado `elem` caminho) = caminho ++ [selecionado]
+            | not (selecionado `elem` map (\(pos, _) -> pos) caminho) = caminho ++ [(selecionado, pai)]
             | otherwise = caminho
         novoVisitados = selecionado : visitados
--- app/Bfs.hs
+++ app/Bfs.hs
@@ -6,27 +6,13 @@ import Data.List
 import Map
 import Types
 
-type Valor = (Point, Point)
-
-bShortestPath:: Point -> Point -> Point
+bShortestPath :: Point -> Point -> Point
 bShortestPath posInicial posFinal = posProx
     where
         caminho = bfs [] (posInicial, posInicial) posFinal [(posInicial, posInicial)]
         posProx = geraProximo caminho posInicial
 
-geraProximo :: [Valor] -> Point -> Point
-geraProximo [] inicial = inicial
-geraProximo [(f,p)] _ = f
-geraProximo [(f,p), _] _ = f
-geraProximo ((f,p):t) inicial = proximo t inicial p
-
-proximo :: [Valor] -> Point -> Point -> Point
-proximo ((f,p):t) inicial anterior
-    | p == inicial && f == anterior = f
-    | f == anterior = proximo t inicial p
-    | otherwise = proximo t inicial anterior
-
-bfs:: [Valor] -> Valor -> Point -> [Valor] -> [Valor]
+bfs :: [MultValor] -> MultValor -> Point -> [MultValor] -> [MultValor]
 bfs visitados (posInicial, _) posFinal fila
     | posAtual == posFinal = novosVisitados
     | not (null filaAdj) = bfs novosVisitados posProx posFinal filaAdj
@@ -36,5 +22,5 @@ bfs visitados (posInicial, _) posFinal fila
         posProx = (posAtual, posAnterior)
         valors2Points = map (\(pos, _) -> pos) visitados
         adjs = ((freeAdjsPoints posAtual) \\ valors2Points)
-        filaAdj = map (\pos -> (pos, posAtual)) adjs
-        novosVisitados = (posAtual, posAnterior):visitados
+        filaAdj = cauda ++ map (\pos -> (pos, posAtual)) adjs
+        novosVisitados = (posAtual, posAnterior) : visitados
