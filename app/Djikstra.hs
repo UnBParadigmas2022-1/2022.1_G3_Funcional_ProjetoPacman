4c2687f72011d371fe807fd7a006eb8ffd5b71c9
Lucas Gomes <lucasglpes@gmail.com>
2022-07-02 10:21:18 -0300

    Refatoração do código

--- app/Djikstra.hs
+++ app/Djikstra.hs
@@ -1,48 +1,3 @@
--- import Data.Heap
--- import Data.List
--- import Graphics.Gloss
-
--- import Map
--- import Types
-
--- -- Insere nó e distância na Heap
--- insereHeap :: Point -> Float -> MinPrioHeap Float Point -> MinPrioHeap Float Point
--- insereHeap posicao peso  heap =
---     Data.Heap.insert (peso, posicao) heap
-
--- menorCaminho :: Point -> Point -> [Point]
--- menorCaminho posInicial posFinal
---     | posInicial == posFinal = [posFinal]
---     | otherwise = caminho
---     where
---         heap = insereHeap posInicial 0 (empty :: MinPrioHeap Float Point)
---         caminho = djikstra posInicial posFinal [] heap
-
--- djikstra :: Point -> Point -> [Point] -> MinPrioHeap Float Point -> [Point]
--- djikstra posInicial posFinal visitados heap
---     | selecionado == posFinal = novosVisitados
---     | not isVazia = djikstra selecionado posFinal novosVisitados novaHeap
---     | otherwise = error "Caminho nao encontrado"
---     where
---         isVazia = Data.Heap.null heap 
---         (selecionado, novaHeap) = calcCaminho mapaAtual visitados posInicial heap
---         -- novosVisitados = visitados ++ [selecionado] 
---         novosVisitados 
---             | not (selecionado `elem` visitados) = visitados ++ [selecionado] 
---             | otherwise = visitados
-
-
-
--- calcCaminho :: Mapa -> [Point] -> Point -> MinPrioHeap Float Point -> (Point, MinPrioHeap Float Point)
--- calcCaminho mapa visitados posInicial heap = do
---     let Just ((pesoSelecionado, selecionado), heapPop) = view heap
---     -- let adjsLivres = (freeAdjsPoints posInicial) \\ map (\(filho, _) -> filho) visitados
---     -- let adjsLivres = (freeAdjsPoints posInicial) \\visitados
---     let adjsLivres = (freeAdjsPoints selecionado) \\visitados
-
---     let novaHeap = foldl (\heap adj -> insereHeap adj ((mapa !! point2Index adj) + pesoSelecionado) heap) heapPop adjsLivres
---     (selecionado, novaHeap)
-
 module Djikstra (djikstraPath) where
 
 import Data.Heap
@@ -54,24 +9,25 @@ import Types
 
 type Valor = (Point, Point)
 
-djikstraPath :: Point -> Point -> Point
-djikstraPath posInicial posFinal = geraProximo (reverse caminho) posInicial
+calcCaminho :: Mapa -> [Valor] -> Point -> MinPrioHeap Float Valor -> (Valor, MinPrioHeap Float Valor)
+calcCaminho mapa visitados posInicial heap = do
+    let Just ((pesoSelecionado, (selecionado, paiSelecionado)), heapPop) = view heap
+    let adjsLivres = (freeAdjsPoints selecionado) \\ map (\(filho, _) -> filho) visitados
+    let novaHeap = foldl (\heap adj -> insereHeap (adj, selecionado) ((mapa !! point2Index adj) + pesoSelecionado) heap) heapPop adjsLivres
+    ((selecionado, paiSelecionado), novaHeap)
+
+djikstra :: Point -> Point -> [Valor] -> MinPrioHeap Float Valor -> [Valor]
+djikstra posInicial posFinal visitados heap
+    | selecionado == posFinal = novosVisitados
+    | not isVazia = djikstra selecionado posFinal novosVisitados novaHeap
+    | otherwise = error "Caminho nao encontrado"
     where
-        caminho = menorCaminho posInicial posFinal
+        isVazia = Data.Heap.null heap 
+        ((selecionado, paiSelecionado), novaHeap) = calcCaminho mapaAtual visitados posInicial heap
+        novosVisitados 
+            | not (selecionado `elem` map (\(filho, _) -> filho) visitados) = visitados ++ [(selecionado, paiSelecionado)] 
+            | otherwise = visitados
 
-geraProximo :: [Valor] -> Point -> Point
-geraProximo [] inicial = inicial
-geraProximo [(f,p)] _ = f
-geraProximo [(f,p), _] _ = f
-geraProximo ((f,p):t) inicial = proximo t inicial p
-
-proximo :: [Valor] -> Point -> Point -> Point
-proximo ((f,p):t) inicial anterior
-    | p == inicial && f == anterior = f
-    | f == anterior = proximo t inicial p
-    | otherwise = proximo t inicial anterior
-
--- Insere nó e distância na Heap
 insereHeap :: Valor -> Float -> MinPrioHeap Float Valor -> MinPrioHeap Float Valor
 insereHeap (posicao, pai) peso  heap =
     Data.Heap.insert (peso, (posicao, pai)) heap
@@ -84,27 +40,19 @@ menorCaminho posInicial posFinal
         heap = insereHeap (posInicial, posInicial) 0 (empty :: MinPrioHeap Float Valor)
         caminho = djikstra posInicial posFinal [] heap
 
-djikstra :: Point -> Point -> [Valor] -> MinPrioHeap Float Valor -> [Valor]
-djikstra posInicial posFinal visitados heap
-    | selecionado == posFinal = novosVisitados
-    | not isVazia = djikstra selecionado posFinal novosVisitados novaHeap
-    | otherwise = error "Caminho nao encontrado"
+proximo :: [Valor] -> Point -> Point -> Point
+proximo ((f,p):t) inicial anterior
+    | p == inicial && f == anterior = f
+    | f == anterior = proximo t inicial p
+    | otherwise = proximo t inicial anterior
+
+geraProximo :: [Valor] -> Point -> Point
+geraProximo [] inicial = inicial
+geraProximo [(f,p)] _ = f
+geraProximo [(f,p), _] _ = f
+geraProximo ((f,p):t) inicial = proximo t inicial p
+
+djikstraPath :: Point -> Point -> Point
+djikstraPath posInicial posFinal = geraProximo (reverse caminho) posInicial
     where
-        isVazia = Data.Heap.null heap 
-        ((selecionado, paiSelecionado), novaHeap) = calcCaminho mapaAtual visitados posInicial heap
-        -- novosVisitados = visitados ++ [selecionado] 
-        novosVisitados 
-            | not (selecionado `elem` map (\(filho, _) -> filho) visitados) = visitados ++ [(selecionado, paiSelecionado)] 
-            | otherwise = visitados
-
-
-
-calcCaminho :: Mapa -> [Valor] -> Point -> MinPrioHeap Float Valor -> (Valor, MinPrioHeap Float Valor)
-calcCaminho mapa visitados posInicial heap = do
-    let Just ((pesoSelecionado, (selecionado, paiSelecionado)), heapPop) = view heap
-    let adjsLivres = (freeAdjsPoints selecionado) \\ map (\(filho, _) -> filho) visitados
-    -- let adjsLivres = (freeAdjsPoints posInicial) \\visitados
-    -- let adjsLivres = (freeAdjsPoints selecionado) \\visitados
-
-    let novaHeap = foldl (\heap adj -> insereHeap (adj, selecionado) ((mapa !! point2Index adj) + pesoSelecionado) heap) heapPop adjsLivres
-    ((selecionado, paiSelecionado), novaHeap)
+        caminho = menorCaminho posInicial posFinal
