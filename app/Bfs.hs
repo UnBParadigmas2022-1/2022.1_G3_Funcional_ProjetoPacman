f841cd215c714c17a30e839b07657f7c5fd77eab
RcleydsonR <rafael.cleydson@gmail.com>
2022-07-01 22:09:35 -0300

    Tradução do algoritmo de Bfs para inglês

--- app/Bfs.hs
+++ app/Bfs.hs
@@ -7,20 +7,20 @@ import Map
 import Types
 
 bShortestPath :: Point -> Point -> Point
-bShortestPath posInicial posFinal = posProx
+bShortestPath initialPos goalPos = nextPos
     where
-        caminho = bfs [] (posInicial, posInicial) posFinal [(posInicial, posInicial)]
-        posProx = geraProximo caminho posInicial
+        path = bfs [] (initialPos, initialPos) goalPos [(initialPos, initialPos)]
+        nextPos = generateNext path initialPos
 
 bfs :: [MultValor] -> MultValor -> Point -> [MultValor] -> [MultValor]
-bfs visitados (posInicial, _) posFinal fila
-    | posAtual == posFinal = novosVisitados
-    | not (null filaAdj) = bfs novosVisitados posProx posFinal filaAdj
-    | otherwise = error "Sem solucao"
+bfs path (initialPos, _) goalPos queue
+    | currentPos == goalPos = generalPath
+    | not (null adjQueue) = bfs generalPath nextPos goalPos adjQueue
+    | otherwise = error "There's no corresponding solution path"
     where
-        ((posAtual, posAnterior):cauda) = fila
-        posProx = (posAtual, posAnterior)
-        valors2Points = map (\(pos, _) -> pos) visitados
-        adjs = ((freeAdjsPoints posAtual) \\ valors2Points)
-        filaAdj = cauda ++ map (\pos -> (pos, posAtual)) adjs
-        novosVisitados = (posAtual, posAnterior) : visitados
+        ((currentPos, previousPos):remainingQueue) = queue
+        nextPos = (currentPos, previousPos)
+        valors2Points = map (\(pos, _) -> pos) path
+        adjs = ((freeAdjsPoints currentPos) \\ valors2Points)
+        adjQueue = remainingQueue ++ map (\pos -> (pos, currentPos)) adjs
+        generalPath = (currentPos, previousPos) : path
